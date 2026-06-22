28bf845a077431a00c01b8e62f61ae0ddf693f82
maslina524 <luk.kirill2804@gmail.com>
2026-06-22 23:33:35 +0300

    Now launches third-party programs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, process::Command};
+
 fn main() {
-    println!("Hello, world!");
+    let argv = env::args().collect::<Vec<String>>();
+    let exec_name = match argv.get(1) {
+        Some(a) => a,
+        None => {
+            eprintln!("Wado: no input arguments provided");
+            std::process::exit(1);
+        }
+    };
+    let exec_argv = &argv[2..];
+
+    let status = Command::new(exec_name)
+        .args(exec_argv)
+        .status();
+
+    match status {
+        Ok(s) => {
+            match s.code() {
+                Some(c) => std::process::exit(c),
+                None => eprintln!("Wado: program did not return an exit status code")
+            }
+        },
+        Err(e) => eprintln!("Wado: failed to get program exit status code: {e}")
+    }
+    std::process::exit(0)
 }
