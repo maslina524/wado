1460d96f0cecaf178cb7f78533973c3480f9f584
maslina524 <luk.kirill2804@gmail.com>
2026-06-23 02:30:28 +0300

    Updated types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,16 @@
 use core::ffi::*;
 
+// Base
+pub type BOOL = i32;
+pub type DWORD = u32;
+
+// Pointers
+pub type PDWORD = *mut DWORD;
+pub type LPVOID = *mut c_void;
+
+// Token
+pub type TOKEN_ACCESS_MASK = u32;
+pub type TOKEN_INFORMATION_CLASS = u32;
+
+// Handles
 pub type HANDLE = *mut c_void;
